d2478ff552006d8e4d893499f90c9c0c9a035e49
unknown <zab_vl@tutby>
2023-03-28 00:10:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+void FillUpArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)   {arr[i] = new Random().Next(0, 100); }
+}
+
+int SumOddPositions(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i+=2)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int[] array = new int[10];
+
+FillUpArray(array);
+Console.WriteLine($"Массив: {String.Join(",", array)}");
+Console.WriteLine($"Сумма значений, стоящих на нечетных позициях: {SumOddPositions(array)}");
